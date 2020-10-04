a92a66f707233b4c95ce3acfe8962b90356d955e
prathambatra123 <er.prathambatra@gmail.com>
2020-10-04 14:15:13 +0800

    changed setting file

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ url=""
 os.environ["HTTP_PROXY"]=url
 os.environ["HTTPS_PROXY"]=url
 os.environ["OUTPUT_DIR"]="logs/"
-os.environ["creds_file"]="C:\\Users\\HP\\Documents\\home\\test\\cred.txt"
+os.environ["creds_file"]="/home/nisha/.aws/credentials"
 os.environ["creds_profile"]="default"
